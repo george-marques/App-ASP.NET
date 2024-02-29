a5d17d5752d37efd05309e199065038ea1bf0d0d
George Marques <georgedanielmarques@gmail.com>
2024-02-29 16:19:06 -0300

    Scaffolding views e controller para fornecedores

---
+++ src/Learning.App/Controllers/FornecedoresController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Learning.App.Data;
+using Learning.App.ViewModels;
+using Learning.Business.Interfaces;
+using AutoMapper;
+using Learning.Business.Models;
+
+namespace Learning.App.Controllers
+{
+    public class FornecedoresController : Controller
+    {
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IMapper _mapper;
+
+
+        public FornecedoresController(IFornecedorRepository fornecedorRepository, IMapper mapper)
+        {
+            _fornecedorRepository = fornecedorRepository;
+            _mapper = mapper;
+        }
+
+        // GET: Fornecedores
+        public async Task<IActionResult> Index()
+        {
+            return View(_mapper.Map<IEnumerable<FornecedorDTO>>(await _fornecedorRepository.ObterTodos()));
+        }
+
+        // GET: Fornecedores/Details/5
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var fornecedorDTO = await ObterFornecedorEndereco(id);
+
+            if (fornecedorDTO == null)
+            {
+                return NotFound();
+            }
+
+            return View(fornecedorDTO);
+        }
+
+        // GET: Fornecedores/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Fornecedores/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(FornecedorDTO fornecedorDTO)
+        {
+            if (!ModelState.IsValid)
+                return View(fornecedorDTO);
+
+            var fornecedor = _mapper.Map<Fornecedor>(fornecedorDTO);
+            await _fornecedorRepository.Adicionar(fornecedor);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Fornecedores/Edit/5
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var fornecedorDTO = await ObterFornecedorProdutosEndereco(id);
+
+            if (fornecedorDTO == null)
+            {
+                return NotFound();
+            }
+
+            return View(fornecedorDTO);
+        }
+
+        // POST: Fornecedores/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, FornecedorDTO fornecedorDTO)
+        {
+            if (id != fornecedorDTO.Id)
+                return NotFound();
+
+            if (!ModelState.IsValid)
+                return View(fornecedorDTO);
+
+            var fornecedor = _mapper.Map<Fornecedor>(fornecedorDTO);
+            await _fornecedorRepository.Atualizar(fornecedor);
+
+            return RedirectToAction(nameof(Index));
+
+
+        }
+
+        // GET: Fornecedores/Delete/5
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var fornecedorDTO = await ObterFornecedorEndereco(id);
+
+            if (fornecedorDTO == null)
+                return NotFound();
+
+            return View(fornecedorDTO);
+        }
+
+        // POST: Fornecedores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var fornecedorDTO = await ObterFornecedorEndereco(id);
+           
+            if (fornecedorDTO == null)
+                return NotFound();
+
+            await _fornecedorRepository.Remover(id);
+            
+            return RedirectToAction(nameof(Index));
+        }
+
+        private async Task<FornecedorDTO> ObterFornecedorEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorDTO>(await _fornecedorRepository.ObterFornecedorEndereco(id));
+        }
+
+        private async Task<FornecedorDTO> ObterFornecedorProdutosEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorDTO>(await _fornecedorRepository.ObterFornecedorProdutosEndereco(id));
+        }
+    }
+}
--- src/Learning.App/ViewModels/ProdutoDTO.cs
+++ src/Learning.App/ViewModels/ProdutoDTO.cs
@@ -9,6 +9,10 @@ namespace Learning.App.ViewModels
         public Guid Id { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [DisplayName("Fornecedor")]
+        public Guid FornecedorId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres.", MinimumLength = 2)]
         public string Nome { get; set; }
 
@@ -16,9 +20,9 @@ namespace Learning.App.ViewModels
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres.", MinimumLength = 2)]
         public string Descricao { get; set; }
-        
+
         public IFormFile ImagemUpload { get; set; }
-        
+
         public string Imagem { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
@@ -30,7 +34,7 @@ namespace Learning.App.ViewModels
         [DisplayName("Ativo?")]
         public bool Ativo { get; set; }
 
-        public FornecedorDTO Fornecedor { get; set; }
+        public FornecedorDTO Fornecedor { get; set; 
 
     }
 }
